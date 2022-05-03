f2bc74044b8395f795163bb2f587f32a71cca06c
Maxim Aleynikov <max.aleyinikov@gmail.com>
2022-05-03 18:57:38 +0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+type HelloProps = {
+    name: string;
+};
+
+const Hello = (props: HelloProps) => {
+    return (
+        <h1>Hello, {props.name}!</h1>
+    );
+};
+
+function withName(WrappedComponent: any) {
+    return class extends React.Component {
+        render() {
+            return (
+                <WrappedComponent name="Mad Max" {...this.props} />
+            );
+        };
+    };
+}
+
+const Greeting = withName(Hello);
+const App = () => (
+    <div>
+        <Greeting />
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
